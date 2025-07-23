8d4a6ded333fb499dffd5cb19c27116bc7ab932e
sravanDwadasi <sravandwadasi@gmail.com>
2025-07-23 13:23:12 +0530

    changed the yml of gateway Service

---
+++ CapStoneProject/gateway-service/src/test/java/com/gateway_service/GatewayTServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayTServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
